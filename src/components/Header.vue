735777224ac741345a447283166faf98dd88928a
fredalbert37 <fredalbert37@outlook.com>
2022-07-05 16:02:10 -0500

    header desktop and mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,58 @@
 <template>
-    <header>
-        
-    </header>
+  <header class="header">
+    <nav class="header__nav">
+      <a href="/" class="header__logo">
+        <span class="header__logo--img"></span>
+        <span class="header__logo--name">
+          A <span>cuánto</span> la <span>luka</span>
+        </span>
+      </a>
+      <ion-icon name="menu-outline" class="header__toggle" id="toggle-menu"></ion-icon>
+      
+      <ul class="nav-menu">
+        <li class="nav-menu__item">
+          <a href="#hero" class="nav-menu__link">Inicio</a>
+        </li>
+        <li class="nav-menu__item">
+          <a href="#about" class="nav-menu__link">Acerca</a>
+        </li>
+        <li class="nav-menu__item">
+          <a href="#contact" class="nav-menu__link">Contacto</a>
+        </li>
+        <li class="nav-menu__item">
+          <a href="#newsletter" class="nav-menu__link">Noticias</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
+
 <script>
+import { ref } from "vue";
 export default {
-    
-}
+  setup() {
+    let links = [
+      {
+        name: "Inicio",
+        href: "#hero",
+      },
+      {
+        name: "Acerca de",
+        href: "#about",
+      },
+      {
+        name: "Contacto",
+        href: "#contact",
+      },
+      {
+        name: "Noticias",
+        href: "#newsletter",
+      },
+    ];
+
+    return {
+      links,
+    };
+  },
+};
 </script>
