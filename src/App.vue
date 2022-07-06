664ad0068c6aebf84f4e25c738f49c531ab83cc7
fredalbert37 <fredalbert37@outlook.com>
2022-07-06 13:05:36 -0500

    mobile design start

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
   import Header from './components/Header.vue';
+import MobileDesign from './components/MobileDesign.vue';
 </script>
 
 <template>
-  
   <Header />
+  <main>
+    <MobileDesign></MobileDesign>
+  </main>
 </template>
---
+++ src/components/MobileDesign.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="mobile__container">
+        <div class="mobile__info-banner">
+            <p>
+                Datos actualizados a las {{ lastUpdate }}
+            </p>
+            <span>
+                (El precio se actualiza cada minuto)
+            </span>
+        </div>
+        <div class="mobile__table-companies">
+            <a href="#" class="buys active">Compra</a>
+            <a href="#" class="sells">Venta</a>
+        </div>
+        <div class="currency">
+            <img src="../assets/img/companies/inkamoney.png" alt="inkamoney">
+            <div class="currency__data">
+                <span class="currency__buy">
+                    S/ 3.7180
+                </span>
+                <span class="currrency__sell">
+                    S/ 3.7330
+                </span>
+                <a target="_blank" class="currency__btn" href="https://www.inkamoney.com/">Ir a</a>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { ref } from 'vue'
+export default {
+    setup() {
+        const lastUpdate = ref(new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds())
+        const update = () => {
+            lastUpdate.value = (new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds())
+        }
+        setInterval(update, 60000)
+        update()
+        return {
+            lastUpdate
+        }
+    }
+}
+</script>
