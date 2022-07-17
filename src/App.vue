e81d9670b5f9c33723386d609bbd3da67d6aeba5
fredalbert37 <fredalbert37@outlook.com>
2022-07-17 00:30:29 -0500

    footer complete and responsive

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
 import MobileDesign from './components/MobileDesign.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Header />
-  <main>
+  
     <MobileDesign></MobileDesign>
-  </main>
+  
+  <Footer />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="footer">
+        <div class="footer__content">
+            <div class="footer__company-description">
+                <img src="/Iconremovebg.png" alt="logo">
+                <div class="text">
+                    A cuanto la luka es un comparador del tipo de cambio de algunas
+                    de las casas de cambio mas importantes del Peru.
+                    Lo que te facilita la busqueda de la mejor opcion para ti.
+                </div>
+            </div>
+            <div class="footer__titles" v-for="item in footer" :key="item.title">
+                <h3>{{item.title}}</h3>
+                <ul v-for="link in item.links" :key="link">
+                    <li><a href="#">{{link}}</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer__copyright">
+            <div class="text">
+                <span>Copyright © 2022</span>
+                &nbsp;<span>All rights reserved.</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    setup() {
+    const footer = [
+        {
+            title:"Datos importantes",
+            links:[
+                "Politica de privacidad",
+                "Terminos y condiciones",
+                "Transparencia",
+            ]
+        },
+        {
+            title:"Contacto",
+            links:[
+                "Contacto",
+                "Soporte",
+                "Acerca de",
+            ]
+        },
+        {
+            title:"Redes sociales",
+            links:[
+                "Facebook",
+                "Twitter",
+                "Instagram",
+            ]
+        },
+    ];
+
+        return {
+            footer
+        }
+    }    
+}
+</script>
