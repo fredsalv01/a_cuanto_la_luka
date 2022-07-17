aed9f662f70c9c4d9d599d02a32ab69a14205a9d
fredalbert37 <fredalbert37@outlook.com>
2022-07-16 19:36:12 -0500

    no footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import Header from './components/Header.vue';
 import MobileDesign from './components/MobileDesign.vue';
-import Footer from './components/Footer.vue';
 </script>
 
 <template>
